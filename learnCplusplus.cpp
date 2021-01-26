ef97eb9772a963c33fb90778df0db5df9571da7c
Sourabh Goel <sourabhindian1@gmail.com>
2021-01-26 19:59:39 +0530

    Operator overloading for new and delete.  - Create a structure to track the current ususage of runtine memory  - Create a temp structure and allocate and deallocate memory through    new and delete.

--- learnCplusplus.cpp
+++ learnCplusplus.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
 
+struct AllocationMetrics {
+    uint32_t TotalAllocated;
+    uint32_t TotalFreed;
+    uint32_t CurrentUsuage() { return TotalAllocated - TotalFreed; }
+};
+
+static AllocationMetrics s_AllocationMetrics;
+
+void* operator new(size_t size)
+{
+    std::cout << "Allocating " << size << " bytes\n";
+    s_AllocationMetrics.TotalAllocated += size;
+    return malloc(size);
+}
+
+void operator delete(void* memory, size_t size)
+{
+    std::cout << "Freeing " << size << " bytes\n";
+    s_AllocationMetrics.TotalFreed += size;
+    free(memory);
+}
+
+struct Object {
+    int x, y, z;
+};
+
+void PrintMemoryUsuage()
+{
+    std::cout << "Memory Usuage : " << s_AllocationMetrics.CurrentUsuage() << " bytes\n";
+}
+
 int main()
 {
-    std::cout << "Learn C++ programming language\n";
+    PrintMemoryUsuage();
+    Object* obj = new Object;
+    obj->x = 1;
+    obj->y = 2;
+    obj->z = 3;
+    std::cout << "x = " << obj->x << " y = " << obj->y << " z = " << obj->z << std::endl;
+    PrintMemoryUsuage();
+
+    delete obj;
+    PrintMemoryUsuage();
+    
+    // Small strings are not allocated on heap
+    std::string smallStr = "Small String";
+
+    // Big strings are allocated on heap
+    std::string bigStr = "Big string is allocated on heap";
+
     return 0;
 }
